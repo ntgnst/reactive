7d1334862c180e953dbc111d1f47b3646b74a0d1
Ali Kadir BAGCIOGLU <alikadirbagcioglu@gmail.com>
2019-05-24 10:38:54 +0300

    saga latest

--- src/redux/middleware/sagaSample.js
+++ src/redux/middleware/sagaSample.js
@@ -1,28 +1,29 @@
-import { put, takeEvery, call, all, fork } from 'redux-saga/effects';
+import { put, takeEvery, call, all, takeLatest } from 'redux-saga/effects';
 import { types } from '../actions/album';
 
 function* helloSaga() {
   console.log('Hello Sagas!');
 }
 
-function* fetchAlbums(action) {
+export function* fetchAlbums(action) {
+  console.log('================================ geldi get all ========================================');
   const data = yield call(fetch('https://jsonplaceholder.typicode.com/albums').then(data => data.json()));
   yield put({ type: types.ALBUM_GET_ALL, payload: data });
 }
 
-function* fetchAlbumsById(action) {
-  console.log('geldi');
+export function* fetchAlbumsById(action) {
+  console.log('================================ geldi byid ========================================');
   yield put({ type: types.ALBUM_GET_BY_ID });
 }
 
-function* watchFetchAlbums() {
+export function* watchFetchAlbums() {
   yield takeEvery(types.ALBUM_GET_ALL, fetchAlbums);
 }
 
-function* watchFetchAlbumsById() {
+export function* watchFetchAlbumsById() {
   yield takeEvery(types.ALBUM_GET_BY_ID, fetchAlbumsById);
 }
 
 export default function* rootSaga() {
-  yield all([helloSaga(), watchFetchAlbums()]);
+  yield all([helloSaga(), watchFetchAlbums(), watchFetchAlbumsById()]);
 }
