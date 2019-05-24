796eb8a705326736042bffff63742ca572206866
Ali Kadir BAGCIOGLU <alikadirbagcioglu@gmail.com>
2019-05-24 10:39:45 +0300

    saga

--- src/redux/middleware/sagaSample.js
+++ src/redux/middleware/sagaSample.js
@@ -5,13 +5,13 @@ function* helloSaga() {
   console.log('Hello Sagas!');
 }
 
-export function* fetchAlbums(action) {
+export function* fetchAlbums() {
   console.log('================================ geldi get all ========================================');
   const data = yield call(fetch('https://jsonplaceholder.typicode.com/albums').then(data => data.json()));
   yield put({ type: types.ALBUM_GET_ALL, payload: data });
 }
 
-export function* fetchAlbumsById(action) {
+export function* fetchAlbumsById(actions) {
   console.log('================================ geldi byid ========================================');
   yield put({ type: types.ALBUM_GET_BY_ID });
 }
